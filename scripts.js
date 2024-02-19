c99b99c477084a6aa239e93cff6e884e22a9e9b6
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-02-19 16:53:28 +0600

    function added and button modify

--- scripts.js
+++ scripts.js
@@ -1,24 +1,58 @@
 
+const seatsElement = document.getElementById("total-seats");
+const totalPriceElement = document.getElementById("total-prices");
+const grandTotalPriceElement = document.getElementById("grand-total");
+const addSeatElement = document.getElementById("add-seat");
+const tableBody = document.getElementById("table-body");
+const buttons = document.querySelectorAll('.sit-button');
 
-function getRandomSetBtn(){
-    const countSeatString = '1234';
-    const numbers = countSeatString.split('');
-    const randomSeats =Math.random()*3;
-    const index = Math.round(randomSeats);
-    const number = numbers[index];
-    console.log(number) 
-    return number;
+
+
+const totalSeats = 40;
+
+let count = 0;
+const perSeatPrice = 550;
+let totalPrice = 0;
+
+
+seatsElement.innerText = totalSeats;
+totalPriceElement.innerText = totalPrice;
+grandTotalPriceElement.innerText = totalPrice;
+addSeatElement.innerText = count;
+
+
+function calculateSeatfunction (added) {
+    return totalSeats - added;
 }
 
-function countSeatList(){
-    const number = getRandomSetBtn();
-     const currentSeatArea = document.getElementById('add-seat');
-    currentSeatArea.innerText = number;
-  
-    setBgColorById(number);
-}
-function setBgColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('bg-lime-500');
-}
 
+// buttons.forEach((button, index) => {
+    for(const button of buttons){
+        button.addEventListener('click', function(){
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+
+        count += 1;
+        if(count > 4){
+            alert('Maximum 4 allowed')
+            return;
+        }
+        totalPrice += perSeatPrice;
+
+        td1.innerText = button.innerText;
+        td2.innerText = 'Economy';
+        td3.innerText = perSeatPrice;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tableBody.appendChild(tr);
+
+        seatsElement.innerText = calculateSeatfunction(count);
+        addSeatElement.innerText = count;
+        totalPriceElement.innerText = totalPrice;
+        grandTotalPriceElement.innerText = totalPrice;
+        button.classList.replace('bg-[#eaebec]', 'bg-[#1dd100]');
+    })
+}
